280c0cd037e2f5cb44ebec67d1203a19092049c7
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-10-16 21:51:40 -0400

    Add a validator for --outdir writability
    (Uses an unsafe block, but I'm willing to risk it since access() shouldn't
    mutate anything, so, if there is a bug, it should cause immediate death
    rather than subtle corruption)

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,7 @@ fn main() {
              .global(true)
              .value_name("PATH")
              .default_value(".")  // XXX: Look for an os.curdir equivalent
-             // TODO: Custom validator to verify writability
-             // https://github.com/kbknapp/clap-rs/blob/master/examples/15_custom_validator.rs
+             .validator(validators::dir_writable)
              .help("Path to parent directory for output file(s)"))
         .arg(Arg::with_name("name")
              .long("name")
--- src/validators.rs
+++ src/validators.rs
@@ -1,6 +1,11 @@
+extern crate libc;
+
 use std::error::Error;
 use std::fs::File;
 use std::io::ErrorKind;
+use std::path::Path;
+
+use self::libc::{access,W_OK};
 
 // TODO: Make this different if built on Windows
 const INVALID_FILENAME_CHARS: &'static str = "/\0";
@@ -19,6 +24,23 @@ pub fn filename_valid(value: String) -> Result<(), String> {
         return Ok(());
     }
     Err(format!("Name contains invalid characters: {}", value))
+}
+
+/// Test that the given path **should** be writable
+/// TODO: Unit test **HEAVILY** (Has unsafe block. Here be dragons!)
+pub fn dir_writable(value: String) -> Result<(), String> {
+    // Test that the path is a directory
+    if !Path::new(&value).is_dir() {
+        return Err(format!("Not a directory: {}", value));
+    }
+
+    // I'm willing to risk this because access() shouldn't mutate anything
+    unsafe {
+        // Test if we **should** be able to write to the given path
+        if access(value.as_ptr() as *const i8, W_OK) == 0 { return Ok(()); }
+    }
+
+    Err(format!("Would be unable to write to destination directory: {}", value))
 }
 
 /// Test that the given path can be opened for reading and adjust failure messages
