97b0a0a8fb5e4a1ea4840ecc9c34bcc0e0c3043b
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-11-26 22:15:41 -0500

    Refactor clap-rs use to add unit testing for clap-rs issue #694

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 #[cfg(feature="nightly")]
 extern crate alloc_system;
 
+/// libstd imports
+use std::borrow::Cow;
+
+/// clap-rs imports
 #[macro_use]
 extern crate clap;
 use clap::{App,AppSettings,Arg,SubCommand};
@@ -11,6 +15,16 @@ mod validators;
 
 // TODO: Allow overriding in a config file
 const DEFAULT_INPATH: &'static str = "/dev/sr0";
+/// Allow different defaults to be passed to unit tests
+struct AppConfig<'a> {
+    inpath: Cow<'a, str>,
+}
+
+impl<'a> Default for AppConfig<'a> {
+    fn default() -> AppConfig<'a> {
+        AppConfig { inpath: Cow::Borrowed(DEFAULT_INPATH) }
+    }
+}
 
 /// # Development Policy
 /// Clap validators for references like filesystem paths (as opposed to self-contained
@@ -20,16 +34,60 @@ const DEFAULT_INPATH: &'static str = "/dev/sr0";
 /// To avoid vulnerabilities based on race conditions or shortcomings in functions like
 /// access() (which will falsely claim "/" is writable), all "reference data" must be
 /// validated (and failures handled) on **every** use.
+///
+/// See Also:
+///  http://blog.ssokolow.com/archives/2016/10/17/a-more-formal-way-to-think-about-validity-of-input-data/
 
 /// Wrapper for Arg::from_usage to deduplicate setting a few things all args have
 /// TODO: Does Clap provide a more proper way to set defaults than this?
 fn arg_from_usage(usage: &str) -> Arg {
     Arg::from_usage(usage)
-        .global(true)
+        .global(true)           // FIXME: clap-rs issue #694
         .empty_values(false)
 }
 
-fn main() {
+#[test]
+/// Can override DEFAULT_INPATH when specifying -i before the subcommand
+fn test_can_override_inpath_before() {
+    let defaults = AppConfig::default();
+    let matches = make_clap_parser(&defaults).get_matches_from(&["rip_media", "-i/", "cd"]);
+    let inpath = matches.value_of("inpath").unwrap();
+    assert!(inpath == "/", "\"-i/ cd\" should have produced \"/\" \
+                                   but actually produced \"{}\"", inpath)
+}
+
+#[test]
+/// Can override DEFAULT_INPATH when specifying -i after the subcommand
+fn test_can_override_inpath_after() {
+    let defaults = AppConfig::default();
+    let matches = make_clap_parser(&defaults).get_matches_from(&["rip_media", "cd", "-i/"]);
+    let inpath = matches.value_of("inpath").unwrap();
+    assert!(inpath == "/", "\"cd -i/\" should have produced \"/\" \
+                                   but actually produced \"{}\"", inpath)
+}
+
+#[test]
+/// Validator doesn't get run on the default inpath if -i was specified
+fn test_only_validates_inpath_to_be_used_before() {
+    let defaults = AppConfig { inpath: Cow::Borrowed("/etc/shadow") };
+    let matches = make_clap_parser(&defaults).get_matches_from(&["rip_media", "-i/", "cd"]);
+    let inpath = matches.value_of("inpath").unwrap();
+    assert!(inpath == "/", "\"cd -i/\" should have produced \"/\" \
+                                   but actually produced \"{}\"", inpath)
+}
+
+#[test]
+/// Validator doesn't get run on the default inpath if -i was specified
+fn test_only_validates_inpath_to_be_used_after() {
+    let defaults = AppConfig { inpath: Cow::Borrowed("/etc/shadow") };
+    let matches = make_clap_parser(&defaults).get_matches_from(&["rip_media", "cd", "-i/"]);
+    let inpath = matches.value_of("inpath").unwrap();
+    assert!(inpath == "/", "\"cd -i/\" should have produced \"/\" \
+                                   but actually produced \"{}\"", inpath)
+}
+
+/// Initialize the clap parser to be used by main() or unit tests
+fn make_clap_parser<'a, 'b>(defaults: &'a AppConfig<'b>) -> App<'a, 'a> where 'a: 'b {
     App::new("rip_media")
         .about("Simple frontend for backing up physical media")
         .version(crate_version!())
@@ -40,7 +98,7 @@ fn main() {
         // -- Common Arguments --
         .args(&[
               arg_from_usage("[inpath] -i --inpath=<PATH>")
-                .default_value(DEFAULT_INPATH)
+                .default_value(&defaults.inpath)
                 .validator(validators::path_readable)
                 .help("Path to source medium (device, image file, etc.)"),
             arg_from_usage("[outdir] -o --outdir=<PATH>")
@@ -92,5 +150,22 @@ fn main() {
             SubCommand::with_name("damaged")
                 .display_order(3)
                 .about("Recover a damaged CD")])
-        .get_matches();
 }
+
+fn main() {
+    // TODO: Move all of this parsing into its own function so I can unit test it
+    let defaults = AppConfig::default();
+    let matches = make_clap_parser(&defaults).get_matches();
+
+    match matches.subcommand_name() {
+        Some(e) => println!("TODO: Implement subcommand: {}", e),
+        None => unreachable!()
+    }
+
+    // TODO:
+    // 1. Do common setup for disc set
+    // 2. For each disc...
+    //      ...call the ripping command appropriate to the subcommand
+}
+
+// vim: set sw=4 sts=4 :
